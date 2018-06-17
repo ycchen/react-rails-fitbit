4fbcf372f159f5622d014ae70085d6e9f8b8119c
Yungchih Chen <yungchih_chen@yahoo.com>
2018-06-17 11:19:17 -0400

    hid the fitbit login link when user loggedIn

--- app/javascript/packs/dashboard.jsx
+++ app/javascript/packs/dashboard.jsx
@@ -4,12 +4,17 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 class Dashboard extends Component {
   constructor(props) {
-    super(props)
+    super(props),
+    this.state = {
+      user: {},
+      loggedIn: false
+    }
   }
   componentDidMount() {
     if(window.location.hash){
       let fitbitToken = window.location.hash.slice(1).split("&")[0].replace("access_token=","")
       console.log('fitbitToken=', fitbitToken )
+      this.setState({loggedIn: true})
     }
   }
   
@@ -21,11 +26,13 @@ class Dashboard extends Component {
           <p className="lead">Your personal fitness dashboardf</p>
         </header>
 
-        <div className="text-center">
-          <a href="https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22CZTC&redirect_uri=http%3A%2F%2Flocalhost%3A3000&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800">
-              Log in with fitbit
-          </a>
-        </div>
+        { !this.state.loggedIn &&
+          <div className="text-center">
+            <a href="https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22CZTC&redirect_uri=http%3A%2F%2Flocalhost%3A3000&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800">
+                Log in with fitbit
+            </a>
+          </div>
+        }
 
       </div>
     );
